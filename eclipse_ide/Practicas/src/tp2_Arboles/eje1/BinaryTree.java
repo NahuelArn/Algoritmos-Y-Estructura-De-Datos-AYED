cb1025558a2cf01454197fcf6c187c48e0070192
¨nahuel¨ <nahuelarancibiauni@gmail.com>
2024-04-05 17:13:43 -0300

    swap

---
+++ eclipse_ide/Practicas/src/tp2_Arboles/eje1/BinaryTree.java
@@ -0,0 +1,99 @@
+package tp2_Arboles.eje1;
+
+public class BinaryTree<T>  {	
+		private T data;
+		private BinaryTree<T> leftChild;   
+		private BinaryTree<T> rightChild; 
+
+		
+		public BinaryTree() {
+			super();//no es necesario usar Super, se puede hacer un constructor vacio
+		}
+
+		public BinaryTree(T data) {
+			this.data = data;
+		}
+
+		public T getData() {
+			return data;
+		}
+		
+		/**@Param setea un valor en el nodo **/
+		public void setData(T data) { 
+			this.data = data;
+		}
+		/**
+		 * Preguntar antes de invocar si hasLeftChild()
+		 * @return
+		 */
+		public BinaryTree<T> getLeftChild() {
+			return leftChild;
+		}
+		/**
+		 * Preguntar antes de invocar si hasRightChild()
+		 * @return
+		 */
+		public BinaryTree<T> getRightChild() {
+			return this.rightChild;
+		}
+
+		public void addLeftChild(BinaryTree<T> child) {
+			this.leftChild = child;
+		}
+
+		public void addRightChild(BinaryTree<T> child) {
+			this.rightChild = child;
+		}
+
+		public void removeLeftChild() {
+			this.leftChild = null;
+		}
+
+		public void removeRightChild() {
+			this.rightChild = null;
+		}
+
+		public boolean isEmpty(){  //si no tiene hijos ni tiene dato en el padre
+			return (this.isLeaf() && this.getData() == null);
+		}
+		/**@return retorna true, si es hoja el nodo actul **/
+		public boolean isLeaf() {	//es hoja
+			return (!this.hasLeftChild() && !this.hasRightChild());
+
+		}
+		/**
+		 * Retorna true, si tiene un hijo izquierdo
+		 * @return
+		 */	
+		public boolean hasLeftChild() {
+			return this.leftChild!=null;
+		}
+		/**
+		 * Retorna true, si tiene un hijo derecho
+		 * @return
+		 */
+		public boolean hasRightChild() {
+			return this.rightChild!=null;
+		}
+		@Override
+		public String toString() {
+			return this.getData().toString();
+		}
+		
+		public  int contarHojas() {
+		   
+			return 0;
+		}
+			
+			
+	    	 
+	    public BinaryTree<T> espejo(){
+			       		  
+	 	   return null;
+	    }
+
+		// 0<=n<=m
+		public void entreNiveles(int n, int m){
+			
+	   }
+}
---
+++ eclipse_ide/Practicas/src/tp2_Arboles/eje1/Pruebarda.java
@@ -0,0 +1,8 @@
+package tp2_Arboles.eje1;
+
+public class Pruebarda {
+	public static void main(String[] args) {
+		BinaryTree<Integer> b = new BinaryTree<Integer>();
+		
+	}
+}
